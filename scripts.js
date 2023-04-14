dc72ffa19927ea2fe2de34d6b1b3d4b97c8b1889
kawanro <32939470+kawanro@users.noreply.github.com>
2023-04-15 00:06:44 +0200

    added functional backend with console output

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
-function add(...args){
+let firstNumber = "";
+let operator = "";
+let operator2 ="";
+let secondNumber = "";
+let arr =[];
+let counter=0;
+const OPERATORS =["+", "-", "*", "/"];
+
+
+//define add function 
+function add(...args){ 
     return args.reduce((total, current) => {
         return total=current+total;
     })
 }
 
+//define subtrat function 
 function subtract(...args){
     return args.reduce((total, current) => {
         return total=total-current;
@@ -11,6 +22,7 @@ function subtract(...args){
     })
 }
 
+//define multiply function 
 function multiply(...args){
     return args.reduce((total, current) => {
         return total=total*current;
@@ -18,6 +30,7 @@ function multiply(...args){
     })
 }
 
+//define divide function 
 function divide(...args){
     if(args.includes(0)){
         return "Cant divide through 0"
@@ -28,10 +41,8 @@ function divide(...args){
     })
 }
 
-let firstNumber = 0;
-let operator = "";
-let secondNumber = 0;
 
+//define operate function which calls math function depening on operator
 function operate(num1, op, num2){
     switch(op){
         case "+":
@@ -45,4 +56,63 @@ function operate(num1, op, num2){
     }
 }
 
-console.log(operate(1,"/",2));
+//define functions when clicked on equals
+
+function equals(){
+    console.log(operate(firstNumber,operator,secondNumber));
+}
+
+//define display function to save current input
+
+function display(input){
+    if(operator==""){
+        firstNumber=firstNumber+input;
+        //console.log(firstNumber);
+    }
+    else {
+        secondNumber = secondNumber+input;
+        //console.log(secondNumber);
+    }
+}
+
+function fillOperator(str){
+
+    if(secondNumber==""){
+
+        operator=str;
+        
+    }
+    else{
+        console.log(operate(firstNumber,operator,secondNumber));
+        firstNumber=operate(firstNumber,operator,secondNumber);
+        secondNumber="";
+        operator=str;
+    }
+
+    
+}
+
+// add eventListener to every Digit and Operator
+const digits = document.querySelectorAll(".digit");
+const operatorButton = document.querySelectorAll(".operator");
+const equalsButton = document.getElementById("equals");
+
+digits.forEach(digit => {
+    digit.addEventListener("click", e => {
+
+            display(e.srcElement.innerText);
+    })
+});
+
+operatorButton.forEach(operator => {
+    operator.addEventListener("click", e => {
+
+            fillOperator(e.srcElement.innerText);
+    })
+});
+
+equalsButton.addEventListener("click",e => {
+    equals();
+})
+
+console.log(digits);
